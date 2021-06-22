168cf8a4b51033dc4755ca160c2717b9dff0735e
Hallef Santos <hallefsantos@gmail.com>
2021-06-21 22:04:25 -0300

    Add border to button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 const Button = ({ children }) => {
   return (
     <a 
-      className="inline-block py-4 px-10 bg-cyan hover:bg-white sm:text-lg text-xl text-white hover:text-cyan font-bold rounded-full transition-colors" 
+      className="inline-block py-2 px-10 bg-cyan hover:bg-white sm:text-lg text-xl text-white hover:text-cyan font-bold rounded-full border-2 border-cyan transition-colors" 
       href="#"
     >
       {children}
